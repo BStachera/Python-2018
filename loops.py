a786f76df78fa6d0000157b388206f1fa595ae36
BStachera <bartlomiejstachera@gmail.com>
2018-10-24 19:58:49 +0200

    Update loops.py

--- loops.py
+++ loops.py
@@ -2,5 +2,12 @@
 a = 3 if 3**2 > 9 else 14
 print (a)
 #loop 
-for x in range(1,10):
-    print("hello")
+a = input("type something")
+
+for x in range(0,len(a),1):
+     if x%3 == 0:
+         print(" ",a[x])
+     elif x%3 == 1:
+         print(2*" ",a[x])
+     elif x%3 == 2:
+         print(3*" ",a[x])
